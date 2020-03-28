d90535607c73edef4fe0613630a4160784a1c4f3
laurabaoye <laurabaoye@gmail.com>
2020-03-27 21:29:06 -0700

    all routes created

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('./public'))
 
 app.use("/", require("./routes/apiroutes"))
+app.use("/", require("./routes/clientsroutes"))
 
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`);
