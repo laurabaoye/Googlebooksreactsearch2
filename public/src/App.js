023edce115a265939ea7d3f446bf46420adbc9e7
laurabaoye <laurabaoye@gmail.com>
2020-03-27 22:29:18 -0700

    html style design

--- public/src/App.js
+++ public/src/App.js
@@ -2,7 +2,28 @@ import React from "react";
 
 function App() {
     return (
-        <div><h1>Welcome to my googlebook react</h1></div>
+        <div>
+            <div className="jumbotron text-center bg-info">
+                <h1 className="display-3 text-warning" style={{ textShadow: '2px 2px 2px #444' }}>Welcome to my googlebook react</h1>
+            </div>
+            <div className="input-group mb-3">
+                <input
+                    onChange={"handleChange"}
+                    className="form-control"
+                    placeholder="Search for"
+                    type="text"
+                />
+                <div className="input-group-append">
+                    <button
+                        onClick={"handleSubmit"}
+                        className="btn btn-outline-secondary"
+                    >
+                        submit
+                        </button>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
