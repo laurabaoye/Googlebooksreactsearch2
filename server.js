d506044cda7175889afbe27bee06a46e78b316b3
laurabaoye <laurabaoye@gmail.com>
2020-03-27 21:13:26 -0700

    created api routes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/", require("./routes/apiroutes"))
+
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`);
 });
